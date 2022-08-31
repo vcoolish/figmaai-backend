241545c068b0ed0e33450b82203382eddbbdbb9c
Vitaliy Yemets <vitali8.emez@gmail.com>
2022-08-31 14:23:44 +0300

    split constructors in NftId

--- src/main/kotlin/com/app/drivn/backend/nft/model/NftId.kt
+++ src/main/kotlin/com/app/drivn/backend/nft/model/NftId.kt
@@ -2,7 +2,15 @@ package com.app.drivn.backend.nft.model
 
 import java.io.Serializable
 
-data class NftId(
-  val id: Long,
-  val collectionId: Long,
-) : Serializable
+class NftId() : Serializable {
+  var id: Long = 0
+  var collectionId: Long = 0
+
+  constructor(
+    id: Long,
+    collectionId: Long,
+  ) : this() {
+    this.id = id
+    this.collectionId = collectionId
+  }
+}

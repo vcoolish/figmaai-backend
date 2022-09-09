54b1b19cbcad9826da972b21d5eeaba9a49e12ec
Nazar <85495047+cryptomanz1@users.noreply.github.com>
2022-09-09 08:41:38 +0500

    Update CarNftRepository.kt

--- src/main/kotlin/com/app/drivn/backend/nft/repository/CarNftRepository.kt
+++ src/main/kotlin/com/app/drivn/backend/nft/repository/CarNftRepository.kt
@@ -3,6 +3,5 @@ package com.app.drivn.backend.nft.repository
 import com.app.drivn.backend.nft.model.CarNft
 import com.app.drivn.backend.nft.model.NftId
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.web.PageableDefault
 
 interface CarNftRepository : JpaRepository<CarNft, NftId>

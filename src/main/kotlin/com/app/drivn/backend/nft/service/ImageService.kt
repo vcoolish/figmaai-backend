7917e78e3ceeb562a2b1839dbb28ca1c147507e0
Viacheslav Kulish <32914239+vcoolish@users.noreply.github.com>
2022-10-23 21:56:18 +0100

    add dispose function

--- src/main/kotlin/com/app/drivn/backend/nft/service/ImageService.kt
+++ src/main/kotlin/com/app/drivn/backend/nft/service/ImageService.kt
@@ -1,10 +1,19 @@
 package com.app.drivn.backend.nft.service
 
+import com.app.drivn.backend.config.properties.AppProperties
 import com.app.drivn.backend.nft.repository.ImageRepository
 import org.springframework.stereotype.Service
 
 @Service
 class ImageService(
   private val imageRepository: ImageRepository,
+  private val appProperties: AppProperties,
 ) {
+
+  fun disposeNextImage(): String {
+    val imageEntry = imageRepository.findAll().first()
+    val id = imageEntry.dataTxId
+    imageRepository.delete(imageEntry)
+    return "${appProperties.arweaveUrl}$id"
+  }
 }

6660aee82cf986bdc548e642f137d4edbc0e5315
Viacheslav Kulish <32914239+vcoolish@users.noreply.github.com>
2023-01-07 14:57:20 +0000

    change mint price

--- src/main/kotlin/com/app/surnft/backend/nft/entity/ImageCollection.kt
+++ src/main/kotlin/com/app/surnft/backend/nft/entity/ImageCollection.kt
@@ -17,8 +17,8 @@ enum class ImageCollection(
 ) {
   SUR(
     collectionId = 0,
-    price = 0.005,
-    mintPrice = 0.05,
+    price = 0.001,
+    mintPrice = 0.01,
     title = "Sur",
     quality = Quality.COMMON,
     minSpeed = 20,

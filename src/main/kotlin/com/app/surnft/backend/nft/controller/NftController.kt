96cbdaa7db97e76198ae4a7ccc87f616e01dea3d
Viacheslav Kulish <32914239+vcoolish@users.noreply.github.com>
2023-01-17 13:38:17 +0000

    add mock upload endpoint

--- src/main/kotlin/com/app/surnft/backend/nft/controller/NftController.kt
+++ src/main/kotlin/com/app/surnft/backend/nft/controller/NftController.kt
@@ -1,6 +1,7 @@
 package com.app.surnft.backend.nft.controller
 
 import com.app.surnft.backend.ai.AiProvider
+import com.app.surnft.backend.ai.UploadRequest
 import com.app.surnft.backend.constraint.Address
 import com.app.surnft.backend.nft.dto.CarLevelUpCostResponse
 import com.app.surnft.backend.nft.dto.GetAllNftRequest
@@ -16,12 +17,7 @@ import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
 import org.springframework.data.web.PageableDefault
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PatchMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestHeader
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 
 @Validated
@@ -111,5 +107,13 @@ class NftController(
   ): Boolean {
     val nft = nftService.mint(address, collectionId, id)
     return nft.isMinted
+  }
+
+  @PostMapping("/upload")
+  fun upload(
+    @Address @RequestHeader address: String,
+    @RequestBody @Valid file: UploadRequest,
+  ): String {
+    return "https://5c657dfydrq5ehdwiuwls3lx76gkxxs4c46dgb7ks5wr6ceeoycq.arweave.net/6L3fjLgcYdIcdkUsuW13_4yr3lwXPDMH6pdtHwiEdgU"
   }
 }

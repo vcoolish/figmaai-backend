e8b2af6bb8bc92ca21279f082df0b17c4bd2716f
Viacheslav Kulish <32914239+vcoolish@users.noreply.github.com>
2023-03-04 18:48:03 -0500

    Custom sequence generator

--- src/main/kotlin/com/app/surnft/backend/nft/model/Nft.kt
+++ src/main/kotlin/com/app/surnft/backend/nft/model/Nft.kt
@@ -2,20 +2,25 @@ package com.app.surnft.backend.nft.model
 
 import com.app.surnft.backend.common.model.AbstractJpaPersistable
 import org.hibernate.Hibernate
+import org.hibernate.annotations.GenericGenerator
+import org.hibernate.engine.spi.SharedSessionContractImplementor
+import org.hibernate.id.IdentifierGenerator
+import org.hibernate.id.enhanced.SequenceStyleGenerator
+import org.springframework.batch.core.Entity
+import java.io.Serializable
 import java.util.*
 import javax.persistence.*
+
 
 @IdClass(NftId::class)
 @MappedSuperclass
 open class Nft : AbstractJpaPersistable<NftId>() {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "image_nfts_id_sequence_gen")
-  @SequenceGenerator(
+  @GeneratedValue(generator = "image_nfts_id_sequence_gen")
+  @GenericGenerator(
     name = "image_nfts_id_sequence_gen",
-    sequenceName = "image_nfts_id_sequence",
-    initialValue = 1_000_000,
-    allocationSize = 1,
+    strategy = "com.app.surnft.backend.nft.model.SetableSequenceGenerator",
   )
   @Column(name = "id", nullable = false)
   var id: Long? = null
@@ -35,7 +40,7 @@ open class Nft : AbstractJpaPersistable<NftId>() {
   @Column(length = 512)
   lateinit var description: String
 
-//  @OneToOne
+  //  @OneToOne
 //  @JoinColumn(name = "image_id")
   lateinit var image: String
 
@@ -54,4 +59,33 @@ open class Nft : AbstractJpaPersistable<NftId>() {
   }
 
   override fun hashCode(): Int = Objects.hash(id, collectionId)
+}
+
+class SetableSequenceGenerator : IdentifierGenerator {
+
+  /**
+   * Custom id generation. If id is set on the
+   * com.curecomp.common.hibernate.api.Entity instance then use the set one,
+   * if id is 'null' or '0' then generate one.
+   */
+  override fun generate(session: SharedSessionContractImplementor, obj: Any): Serializable {
+    return if ((obj as? Entity)?.id == null) {
+      val query = String.format(
+        "select %s from %s",
+        session.getEntityPersister(obj.javaClass.name, obj)
+          .identifierPropertyName,
+        obj.javaClass.simpleName
+      )
+
+      val ids = session.createQuery(query, Long::class.java).stream()
+
+      val max: Long = ids.max { o1, o2 ->
+        o1.compareTo(o2)
+      }.orElse(1000000L)
+
+      return max + 1
+    } else {
+      obj.id
+    }
+  }
 }

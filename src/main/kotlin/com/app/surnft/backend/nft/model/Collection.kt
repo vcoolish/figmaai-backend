d76162486179e134711201ce1b8635efbc01a2c6
Viacheslav Kulish <32914239+vcoolish@users.noreply.github.com>
2023-03-09 20:02:13 -0500

    fix collection creation

--- src/main/kotlin/com/app/surnft/backend/nft/model/Collection.kt
+++ src/main/kotlin/com/app/surnft/backend/nft/model/Collection.kt
@@ -1,5 +1,6 @@
 package com.app.surnft.backend.nft.model
 
+import com.app.surnft.backend.common.model.AbstractJpaPersistable
 import com.app.surnft.backend.user.model.User
 import org.hibernate.annotations.CreationTimestamp
 import org.springframework.data.annotation.CreatedDate
@@ -8,10 +9,12 @@ import javax.persistence.*
 
 @Entity
 @Table(name = "collections")
-class Collection {
+class Collection : AbstractJpaPersistable<Long>() {
 
   @Id
   var id: Long = System.currentTimeMillis() / 1000
+
+  override fun getId(): Long = id
 
   lateinit var address: String
 

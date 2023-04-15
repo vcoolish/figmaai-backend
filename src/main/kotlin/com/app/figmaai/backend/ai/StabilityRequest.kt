09091d10b86ab5edd0c327b206dc7a8e2b83dd4b
Viacheslav Kulish <32914239+vcoolish@users.noreply.github.com>
2023-04-14 23:48:29 -0500

    increase resolution

--- src/main/kotlin/com/app/figmaai/backend/ai/StabilityRequest.kt
+++ src/main/kotlin/com/app/figmaai/backend/ai/StabilityRequest.kt
@@ -1,5 +1,5 @@
 package com.app.figmaai.backend.ai
 
-data class StabilityRequest(val text_prompts: List<StabilityPrompt>)
+data class StabilityRequest(val text_prompts: List<StabilityPrompt>, val height: Int = 1024, val width: Int = 1024)
 data class StabilityImageRequest(val text_prompts: List<StabilityPrompt>, val init_image: ByteArray)
 data class StabilityPrompt(val text: String)

1e7cf451daabc9f84d16f63e8a0a075e8e429958
Viacheslav Kulish <32914239+vcoolish@users.noreply.github.com>
2023-04-22 21:54:27 -0500

    add cookie

--- src/main/kotlin/com/app/figmaai/backend/subscription/PaypalSubscriptionValidator.kt
+++ src/main/kotlin/com/app/figmaai/backend/subscription/PaypalSubscriptionValidator.kt
@@ -24,6 +24,7 @@ class PaypalSubscriptionValidator(
     headers.contentType = MediaType.APPLICATION_JSON
 //    headers.add("X-PAYPAL-SECURITY-CONTEXT", "{\"consumer\":{\"accountNumber\":1181198218909172527,\"merchantId\":\"5KW8F2FXKX5HA\"},\"merchant\":{\"accountNumber\":1659371090107732880,\"merchantId\":\"2J6QB8YJQSJRJ\"},\"apiCaller\":{\"clientId\":\"AdtlNBDhgmQWi2xk6edqJVKklPFyDWxtyKuXuyVT-OgdnnKpAVsbKHgvqHHP\",\"appId\":\"APP-6DV794347V142302B\",\"payerId\":\"2J6QB8YJQSJRJ\",\"accountNumber\":\"1659371090107732880\"},\"scopes\":[\"https://api-m.paypal.com/v1/subscription/.*\",\"https://uri.paypal.com/services/subscription\",\"openid\"]}");
     headers.add("Accept", "application/json")
+    headers.add("Cookie", "l7_az=ccg14.slc")
     headers.setBasicAuth(appProperties.paypalId, appProperties.paypalSecret)
 
     val response = restTemplate.getForEntity(

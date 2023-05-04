50d6dc8b4c8ec688c4d26e266a6f87797352c5ae
Viacheslav Kulish <32914239+vcoolish@users.noreply.github.com>
2023-05-04 17:04:36 -0500

    increase energy spend

--- src/main/kotlin/com/app/figmaai/backend/ai/AiProvider.kt
+++ src/main/kotlin/com/app/figmaai/backend/ai/AiProvider.kt
@@ -1,9 +1,9 @@
 package com.app.figmaai.backend.ai
 
 enum class AiProvider(val energy: Int) {
-  DALLE(6),
-  MIDJOURNEY(6),
-  STABILITY(6)
+  DALLE(10),
+  MIDJOURNEY(10),
+  STABILITY(10),
 }
 
 enum class AiVersion {

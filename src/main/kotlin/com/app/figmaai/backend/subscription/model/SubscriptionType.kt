e1521e77b87fb31c478d3df1ca07268610a23564
Viacheslav Kulish <32914239+vcoolish@users.noreply.github.com>
2023-06-10 14:09:47 -0500

    add test link

--- src/main/kotlin/com/app/figmaai/backend/subscription/model/SubscriptionType.kt
+++ src/main/kotlin/com/app/figmaai/backend/subscription/model/SubscriptionType.kt
@@ -1,7 +1,8 @@
 package com.app.figmaai.backend.subscription.model
 
 enum class SubscriptionType(val lemonId: String, val tokens: Int, val slug: String) {
-  Basic_Month("81235", 300, "d035b962-b455-44bc-804c-dcb0cc52aae8"),
+//  Basic_Month("81235", 300, "d035b962-b455-44bc-804c-dcb0cc52aae8"),
+  Basic_Month("81235", 300, "9eeb76f6-bc13-455b-92d3-9a6a220b22ce"),
   Standard_Month("81225", 500, "427e9c1b-d1a0-4de1-a75f-51606b4fefed"),
   Advanced_Month("81226", 800, "3bf34566-7c9c-4acc-b866-db94a063b047"),
   Basic_Year("85009", 300, "d1daf6d3-5c71-4c3c-a34f-c1528153b80d"),
@@ -10,6 +11,7 @@ enum class SubscriptionType(val lemonId: String, val tokens: Int, val slug: Stri
 
   fun getLink() =
     SubscriptionLink(
+//      url = "https://aidsnpro.lemonsqueezy.com/checkout/buy/${slug}",
       url = "https://aidsnpro.lemonsqueezy.com/checkout/buy/${slug}",
       generations = tokens.toString(),
       name = name,

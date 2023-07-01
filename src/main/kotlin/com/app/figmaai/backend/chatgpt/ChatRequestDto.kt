3213fcaf7692cd8201d533b4f0c8e5a271a78c8d
Viacheslav Kulish <32914239+vcoolish@users.noreply.github.com>
2023-06-30 21:35:57 -0400

    improve ujm response

--- src/main/kotlin/com/app/figmaai/backend/chatgpt/ChatRequestDto.kt
+++ src/main/kotlin/com/app/figmaai/backend/chatgpt/ChatRequestDto.kt
@@ -94,7 +94,7 @@ enum class UxMode(val value: String, val title: String, val inputs: Map<String, 
     {
       "stages": [
         {
-          "stage": "Stage 1",
+          "stage": "Stage 1 title",
           "actions": [
             "Action 1",
             "Action 2"
@@ -121,7 +121,7 @@ enum class UxMode(val value: String, val title: String, val inputs: Map<String, 
           ]
         },
         {
-          "stage": "Stage 2",
+          "stage": "Stage 2 title",
           "actions": [
             "Action 1",
             "Action 2"

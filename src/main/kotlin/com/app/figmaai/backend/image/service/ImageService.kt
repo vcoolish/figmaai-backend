d2cd225c6fa0e7814d533a48a0fee36d0b067654
Viacheslav Kulish <32914239+vcoolish@users.noreply.github.com>
2023-07-30 14:28:11 -0600

    strength

--- src/main/kotlin/com/app/figmaai/backend/image/service/ImageService.kt
+++ src/main/kotlin/com/app/figmaai/backend/image/service/ImageService.kt
@@ -194,7 +194,7 @@ class ImageService(
   ) {
     val cleanPrompt = if (prompt.startsWith("https://")) prompt.substringAfter(" ") else prompt
 
-    val images = createStabilityImage("${initEntity.image} $prompt", height, width, 80, 10)
+    val images = createStabilityImage("${initEntity.image} $prompt", height, width, 60, 10)
     logger.info("images ${images.size}")
     val file = File.createTempFile(UUID.randomUUID().toString(), ".gif")
     val output = FileImageOutputStream(file)
